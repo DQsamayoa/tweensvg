3497e81e94ffb68c89e1237bd62563f9e0330ad2
Daniel Bailey <danieljabailey@gmail.com>
2017-07-16 18:17:29 +0100

    Got fade in and fade out delay sequencing to work

--- TweenSVG/Tweener.py
+++ TweenSVG/Tweener.py
@@ -1,10 +1,10 @@
 from copy import deepcopy
 import itertools
-import re
 from defusedxml.ElementTree import parse
 from xml.etree import ElementTree as ElementTreeModule
 from xml.etree.ElementTree import ElementTree # Dr Watson
 from xml.etree.ElementTree import Element
+import re
 
 ElementTreeModule.register_namespace('', "http://www.w3.org/2000/svg")
 
@@ -29,17 +29,41 @@ def _value_unit(string):
 def _to_unit_val(value, unit):
     return "%f%s"%(value, unit)
 
+def _viewbox_vals(string):
+    m = re.match(r"(\d+(?:\.\d)?\d*) *[, ] *(\d+(?:\.\d)?\d*) *[, ] *(\d+(?:\.\d)?\d*) *[, ] *(\d+(?:\.\d)?\d*)", string)
+    if not m:
+        raise ValueError("invalid viewbox string")
+    groups = m.groups()
+    return tuple(float(groups[i]) for i in range(4))
+
+def _to_viewbox_val(left, top, width, height):
+    return "%f %f %f %f" % (left, top, width, height)
+
+def _transforms(string):
+    matches = re.findall(r"(translate|rotate|scale|matrix|skew(?:x|y))\(([^)]+)\)", string)
+    return {transform: args for transform, args in matches}
+
 class Tweener():
-    def __init__(self, duration="5s", group_matching=False):
+    def __init__(self, duration="5s", group_matching=False, fadein_late=False, fadeout_early=False):
         self.duration = duration
+        self.fadein_duration = "1s"
+        self.fadeout_duration = "1s"
+        self.fadein_late = fadein_late
+        self.fadeout_early = fadeout_early
         self.group_matching = group_matching
         self.maxwidth = 0
         self.maxheight = 0
+        self.min_vb_top = 0
+        self.min_vb_left = 0
+        self.max_vb_width = 0
+        self.max_vb_height = 0
         self.widthunit = None
         self.heightunit = None
         self.keyframes = []
+        self.animation_number = 0
 
     def add_keyframe(self, keyframe):
+        """ Add a keyframe to the animation. Units must match other frames """
         if not isinstance(keyframe, ElementTree):
             raise TypeError("keyframe must be an ElementTree object")
         self.keyframes.append(keyframe)
@@ -50,17 +74,22 @@ class Tweener():
                 self.widthunit = widthunit
             else:
                 raise ValueError("Mixed units in keyframe dimensions")
+            self.maxwidth = max(self.maxwidth, width)
         if 'height' in root_attrs:
             height, heightunit = _value_unit(root_attrs['height'])
             if self.heightunit is None or self.heightunit == heightunit:
                 self.heightunit = heightunit
             else:
                 raise ValueError("Mixed units in keyframe dimensions")
+            self.maxheight = max(self.maxheight, height)
 
-        self.maxwidth = max(self.maxwidth, width)
-        self.maxheight = max(self.maxheight, height)
-
-        print(root_attrs)
+        if 'viewBox' in root_attrs:
+            vb = root_attrs['viewBox']
+            left, top, width, height = _viewbox_vals(vb)
+            self.min_vb_top = min(self.min_vb_top, top)
+            self.min_vb_left = min(self.min_vb_left, left)
+            self.max_vb_width = max(self.max_vb_width, width)
+            self.max_vb_height = max(self.max_vb_height, height)
 
     def add_keyframe_from_file(self, filename):
         self.add_keyframe(parse(filename))
@@ -84,38 +113,87 @@ class Tweener():
         return anim_from, anim_to
             
 
-    def _animate_tags(self, from_attrs, to_attrs):
+    def _animate_tags_custom(self, from_attrs, to_attrs, begin=None, eid=None, dur=None):
+        def common_attrs(animtag):
+            if begin is not None:
+                animtag.attrib['begin'] = begin
+                animtag.attrib['fill'] = 'freeze'
+            if eid is None:
+                animtag.attrib['id'] = "tween_%d" % (self.animation_number)
+                self.animation_number += 1
+            else:
+                animtag.attrib['id'] = eid
+        if dur is None:
+            dur = self.duration
         for attr, from_val in from_attrs.items():
             to_val = to_attrs[attr]
-            animtag = Element("animate",
-                 {
-                    "attributeType": "XML",
-                    "attributeName": attr,
-                    "from": from_val,
-                    "to": to_val,
-                    "dur": self.duration,
-                    "repeatCount": "indefinite"
-                })
-            yield animtag
+            if attr == 'transform':
+                # Transforms are handled with animateTransform tags
+                from_transforms = _transforms(from_val)
+                to_transforms = _transforms(to_val)
+                for trans_type, from_args in from_transforms.items():
+                    if trans_type in to_transforms:
+                        to_args = to_transforms[trans_type]
+                        if from_args != to_args:
+                            animtag = Element("animateTransform",
+                                {
+                                    "attriuteType": "XML",
+                                    "attributeName": "transform",
+                                    "type": trans_type,
+                                    "from": from_args,
+                                    "to": to_args,
+                                    "dur": dur,
+                                    #"repeatCount": "indefinite"
+                                })
+                            common_attrs(animtag)
+                            yield animtag
+            else:
+                animtag = Element("animate",
+                     {
+                        "attributeType": "XML",
+                        "attributeName": attr,
+                        "from": from_val,
+                        "to": to_val,
+                        "dur": dur,
+                        #"repeatCount": "indefinite"
+                    })
+                common_attrs(animtag)
+                yield animtag
 
-    def _fade_animation(self, direction):
+    def _animate_tags(self, from_attrs, to_attrs):
+        return self._animate_tags_custom(from_attrs, to_attrs, begin="tween_transition.begin")
+
+    def _fade_animation(self, direction, opacity, begin=None, dur=None):
         if direction not in {-1, 1}:
             raise ValueError("Direction must be 1 or -1")
-        (fromval, toval) = ("1", "0") if direction == -1 else ("0", "1")
+        (fromval, toval) = (opacity, "0") if direction == -1 else ("0", opacity)
         from_attr = {"opacity": fromval}
         to_attr = {"opacity": toval}
-        return self._animate_tags(from_attr, to_attr)
+        return self._animate_tags_custom(from_attr, to_attr, begin=begin, dur=dur)
 
-    def _fade_in_animation(self):
-        return self._fade_animation(1)
+    def _fade_in_animation(self, opacity):
+        return self._fade_animation(1, opacity, begin="tween_fadein.begin", dur=self.fadein_duration)
 
-    def _fade_out_animation(self):
-        return self._fade_animation(-1)
+    def _fade_out_animation(self, opacity):
+        return self._fade_animation(-1, opacity, begin="tween_fadeout.begin", dur=self.fadeout_duration)
+
+    def _fade_out_element(self, element):
+        opacity = element.attrib.get("opacity", "1")
+        element.attrib['opacity'] = opacity
+        return self._fade_out_animation(opacity)
+
+
+    def _fade_in_element(self, element):
+        opacity = element.attrib.get("opacity", "1")
+        element.attrib['opacity'] = "0"
+        return self._fade_in_animation(opacity)
 
     def _tween_elements(self, from_element: Element, to_element: Element, group_merge=False):
         result_element = Element(from_element.tag, from_element.attrib)
         result_element.text = from_element.text
         result_element.tail = from_element.tail
+
+
         done_ids = []
         merged_to_elements = []
         for sub_from_element in from_element:
@@ -143,8 +221,9 @@ class Tweener():
                                 break
                     if not found:
                         # Couldn't merge, just fade out...
-                        anim_tags = self._fade_out_animation()
                         tweened_sub_element = deepcopy(sub_from_element)
+                        #anim_tags = self._fade_out_animation()
+                        anim_tags = self._fade_out_element(tweened_sub_element)
             else:
                 done_ids.append(eid)
                 tagname = sub_from_element.tag
@@ -159,8 +238,9 @@ class Tweener():
                         sub_to_element = maybe_sub_to_element
                 if sub_to_element is None:
                     # No mathching element in from, animate fade out
-                    anim_tags = self._fade_out_animation()
+                    #anim_tags = self._fade_out_animation()
                     tweened_sub_element = deepcopy(sub_from_element)
+                    anim_tags = self._fade_out_element(tweened_sub_element)
                 else:
                     from_attrs, to_attrs = self._attr_diff(sub_from_element.attrib, sub_to_element.attrib)
                     anim_tags = self._animate_tags(from_attrs, to_attrs)
@@ -175,15 +255,19 @@ class Tweener():
             if ((eid is None) and group_merge and (sub_to_element not in merged_to_elements)) or (eid is not None and eid not in done_ids):
                 # This is a new element, fade it in
                 fade_in_element = deepcopy(sub_to_element)
-                for anim in self._fade_in_animation():
+                for anim in self._fade_in_element(fade_in_element):
                     fade_in_element.append(anim)
                 result_element.append(fade_in_element)
         return result_element
 
-    def _tween(self, from_svg, to_svg):
+    def _tween(self, from_svg, to_svg, extras=None):
         element = self._tween_elements(from_svg.getroot(), to_svg.getroot())
         element.attrib['width'] = _to_unit_val(self.maxwidth, self.widthunit)
         element.attrib['height'] = _to_unit_val(self.maxheight, self.heightunit)
+        element.attrib['viewBox'] = _to_viewbox_val(self.min_vb_left, self.min_vb_top, self.max_vb_width, self.max_vb_height)
+        if extras is not None:
+            for extra in extras:
+                element.append(extra)
         result = ElementTree(element=element)
         return result
 
@@ -204,9 +288,46 @@ class Tweener():
                 del element.attrib[replace]
             self._namespace_fixup(element)
 
+    def _sync_element(self):
+        # Create an invisible dummy element to contain
+        # root animations for synchronisation
+        invisible = Element("g", {"opacity": "0"})
+        text = Element("text", {"y": "20", "opacity":"0"})
+        text.text = "Test"
+        common_attrs = {"attributeName": "opacity", "attributeType": "XML", "from":"0", "to":"1"}
+        fadeout_attribs = {"id": "tween_fadeout",
+                           "begin": "0s",
+                           "dur": self.fadeout_duration,
+                           **common_attrs}
+        transition_attribs = {"id": "tween_transition",
+                              "begin": "0s; tween_fadein.end",
+                              "dur": self.duration,
+                              **common_attrs}
+        fadein_attribs = {"id": "tween_fadein",
+                          "begin": "tween_transition.start",
+                          "dur": self.fadein_duration,
+                          **common_attrs}
+        if self.fadeout_early:
+            # Delay the main transitions until fadout had ended
+            transition_attribs['begin'] = "tween_fadeout.end"
+        if self.fadein_late:    
+            fadein_attribs['begin'] = "tween_transition.end"
+
+        #if not self.fadein_late and not self.fadeout_early:
+        #    transition_attribs['repeatCount'] = 'indefinite'
+        start_fadein = Element("animate", fadein_attribs)
+        start_transition = Element("animate", transition_attribs)
+        start_fadeout = Element("animate", fadeout_attribs)
+        text.append(start_fadein)
+        text.append(start_transition)
+        text.append(start_fadeout)
+        invisible.append(text)
+        return invisible
+
     def tweens(self):
         for a, b in pairwise(self.keyframes):
-            tween = self._tween(a, b)
+            sync_element = self._sync_element()
+            tween = self._tween(a, b, extras=[sync_element])
             self._namespace_fixup([tween.getroot()])
             yield tween
             
--- TweenSVG/__init__.py
+++ TweenSVG/__init__.py
@@ -1,8 +1,8 @@
 
 from TweenSVG.Tweener import Tweener
 
-def tween_svgs_from_filenames(filenames, duration='5s', group_matching=False):
-    tween = Tweener(duration=duration, group_matching=group_matching)
+def tween_svgs_from_filenames(filenames, duration='5s', group_matching=False, fadeout_early=False, fadein_late=False):
+    tween = Tweener(duration=duration, group_matching=group_matching, fadein_late=fadein_late, fadeout_early=fadeout_early)
     for filename in filenames:
         tween.add_keyframe_from_file(filename)
     return tween.tweens()
